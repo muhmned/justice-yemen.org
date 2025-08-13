f3249c8a90a81f4c581a0f8fede8e5c12c0ee8dd
mohammed razzaz <rzazm819@gmail.com>
2025-08-13 16:22:17 +0300

    تعديل الإعدادات

--- backend/src/scripts/testAddArticle.js
+++ backend/src/scripts/testAddArticle.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 const FormData = require('form-data');
 
-const API_URL = 'https://justice-org.onrender.com/api/articles';
+const API_URL = 'http://localhost:5000/api/articles';
 const ADMIN_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJmMDQ0MTY4NC00NjcxLTQxNWItOGRjZC05NGQwNGI4OGYwZWQiLCJ1c2VybmFtZSI6ImFkbWluIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNzUyMDcwMDc1LCJleHAiOjE3NTIxNTY0NzV9.iTdF9NCeIqczTZoHgDF8P5OaV0lkvIloIvtlhE6Olgo';
 
 async function testAddArticle() {
